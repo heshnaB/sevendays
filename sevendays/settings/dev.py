4e008743c7e7a696d318ce0936c60b5bf56ec6d1
heshnaB <heshna599@gmail.com>
2024-04-28 17:13:54 -0600

    Uploading files

---
+++ sevendays/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-eqd2(&sv%z3^#n_y*8z)j3rt7sm&akc_5t)$h9ayv0z^u1$uuq"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
